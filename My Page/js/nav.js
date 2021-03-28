110ca345bba0e1a7e93ee4785f761341cf64d87e
Varada-D <varadada@rediffmail.com>
2021-03-28 12:04:36 +0530

    This is my first portfolio site, created as a part of the "Web Development for Everybody" specialization capstone by University of Michigan (offered through Coursera). I hoe you like it. Do let me know your valuable feedback.

---
+++ My Page/js/nav.js
@@ -0,0 +1,13 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
+function myFunction() {
+	var x = document.getElementById("navbar");
+
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	}
+
+	else {
+		x.className = "topnav";
+	}
+}
